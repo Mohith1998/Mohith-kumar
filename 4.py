5ba4e46c4a80b75561b60db896d6713f65c428dd
Mohith1998 <40819079+Mohith1998@users.noreply.github.com>
2018-07-04 19:35:09 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,8 @@
+x = input("Enter any character: ");
+if x == '0':
+    exit();
+else:
+    if((x>='a' and x<='z') or (x>='A' and x<='Z')):
+    	print(x, "is an alphabet.");
+    else:
+    	print(x, "is not an alphabet.");
